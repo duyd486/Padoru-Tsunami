e5dcddb819b2dcd76c8d86cfc3f58f4e3d8664b6
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-05-24 23:13:20 +0700

    Update SoundManager, coin sound

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -16,12 +16,17 @@ public class SoundManager : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         ZumbaiManager.Instance.OnZumbaiExplosed += ZumbaiManager_OnZumbaiExplosed;
+        ScoreManager.Instance.OnCandyChanged += ScoreManager_OnCandyChanged;
+    }
 
+    private void ScoreManager_OnCandyChanged(object sender, System.EventArgs e)
+    {
+        PlaySound(audioClipRefsSOl.candy, 0.1f);
     }
 
     private void ZumbaiManager_OnZumbaiExplosed(object sender, System.EventArgs e)
     {
-        PlaySound(audioClipRefsSOl.bomb, 0.2f);
+        PlaySound(audioClipRefsSOl.bomb, 0.3f);
     }
 
     private void PlaySound(AudioClip[] audioClipArr, float volume = 1f)
